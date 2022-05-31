610595282be041b2ab78960ec5bda4498f06b977
Bookcliff <bookcliff76@gmail.com>
2022-05-31 17:58:56 -0500

    update chatId

--- api/bot.js
+++ api/bot.js
@@ -4,8 +4,8 @@ module.exports = async (request, response) => {
   const message = `Family Meeting
     Sundays · 2:00 – 3:00pm
     https://meet.google.com/fks-ssrt-fno`;
-    
-  const chatId = -617377330;
+
+  const chatId = -244844478;
 
   const result = await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${chatId}&text=${message}`);
   
