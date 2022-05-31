51c78b566605240499fa495a9d2a2b899700ed58
Bookcliff <bookcliff76@gmail.com>
2022-05-30 19:09:06 -0500

    add cron

--- api/bot.js
+++ api/bot.js
@@ -2,9 +2,9 @@ const fetch = require('node-fetch');
 
 module.exports = async (request, response) => {
   const message = `Family Meeting
-  Sunday, June 5 · 2:00 – 3:00pm
-  Google Meet joining info
-  Video call link: https://meet.google.com/fks-ssrt-fno`;
+    Sundays · 2:00 – 3:00pm
+    Google Meet joining info
+    Video call link: https://meet.google.com/fks-ssrt-fno`;
   const chatId = -244844478;
 
   const result = await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${chatId}&text=${message}`);
