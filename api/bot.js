2b81c7e97a2bfd49d2dc53d2e65169cb9c8eed44
Bookcliff <bookcliff76@gmail.com>
2022-05-31 14:11:54 -0500

    cleaned docs

--- api/bot.js
+++ api/bot.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 module.exports = async (request, response) => {
   const message = `Family Meeting
     Sundays · 2:00 – 3:00pm
-    Google Meet joining info
-    Video call link: https://meet.google.com/fks-ssrt-fno`;
+    https://meet.google.com/fks-ssrt-fno`;
+    
   const chatId = -617377330;
 
   const result = await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${chatId}&text=${message}`);
